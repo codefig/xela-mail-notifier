a39a0634c98491869cb2096c9a8ca2a48af7b64c
Moshood Alaran <geekyshood@gmail.com>
2020-05-23 16:03:51 +0100

    Changed redis configuration

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -1,12 +1,9 @@
+const client = require("redis").createClient(process.env.REDIS_URL);
 const RedisMQ = require("rsmq");
 const uri = require("url");
 const parsed = uri.parse(process.env.REDIS_URL);
 if (process.env.NODE_ENV == "production") {
-  module.exports.rsmq = new RedisMQ({
-    host: parsed.hostname,
-    port: parsed.port,
-    ns: "ns",
-  });
+  module.exports.rsmq = new RedisMQ({ client: client });
 } else {
   module.exports.rsmq = new RedisMQ({
     host: process.env.REDIS_HOST,
